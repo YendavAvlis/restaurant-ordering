ad1c6e240adc28899b5e6d4e8b57745ac1b1437b
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-08-23 00:00:21 +0200

    Menu Items Added

---
+++ index.js
@@ -0,0 +1,28 @@
+import { menuArray } from "./data.js";
+
+const itemList = document.querySelector('#item-list')
+
+function getItems(arr){
+    arr.map((item) => {
+        return itemList.innerHTML += `
+            <div class="item">
+                <div>
+                    <span class="emoji">${item.emoji}</span>
+                    <div class="item-description">
+                        <h2>${item.name}</h1>
+                        <p>${item.ingredients}</p>
+                        <h3>$${item.price}</h3>
+                    </div>
+                </div>
+
+                <button><img src="./images/add-btn.svg" alt=""></button>
+            </div>
+        `
+    })
+}
+
+function render(){
+    getItems(menuArray)
+}
+
+render()
